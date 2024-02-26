3c0679319bde4504c42044a65df2051af63165c5
HACKER X NETWORK <159551041+HackerXNetwork@users.noreply.github.com>
2024-02-26 23:07:22 +0530

    Update bot.py

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -29,7 +29,7 @@ class Anony(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username} \n ᴜsᴇʀɴᴀᴍᴇ : @O_P_Hacker",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
