048db970f95c173a31f8351f8b07f8f796e4980b
Nidra07 <161415110+Nidra07@users.noreply.github.com>
2024-03-16 01:17:02 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,7 +51,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("x_x0Rishi")
+                await self.one.join_chat("earning_model")
                 await self.one.join_chat("Hacker_fed")
             except:
                 pass
@@ -72,7 +72,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Rudra_ni07")
+                await self.two.join_chat("earning_model")
                 await self.one.join_chat("Hacker_fed")
             except:
                 pass
@@ -93,7 +93,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Rudra_ni07")
+                await self.three.join_chat("earning_model")
                 await self.one.join_chat("Hacker_fed")
             except:
                 pass
@@ -114,7 +114,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Rudra_ni07")
+                await self.four.join_chat("earning_model")
                 await self.one.join_chat("Hacker_fed")
             except:
                 pass
@@ -135,7 +135,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Rudra_ni07")
+                await self.five.join_chat("earning_model")
                 await self.one.join_chat("Hacker_fed")
             except:
                 pass
